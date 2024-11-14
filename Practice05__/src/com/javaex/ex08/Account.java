ecd5ef771df86210b9e9f1e998f74460d59c61ae
Kim-kym <msgim725@gmail.com>
2024-11-14 18:09:22 +0900

    ex08

--- Practice05__/src/com/javaex/ex08/Account.java
+++ Practice05__/src/com/javaex/ex08/Account.java
@@ -6,7 +6,44 @@ public class Account {
     private int balance;
     
     //생성자 작성
+    public Account(String accountNo) {
+    	this.accountNo = accountNo;
+    }
     
+    public Account(String accountNo, int balance) {
+    	this.accountNo = accountNo;
+    	this.balance = balance; 		
+    }
+    
+    public String getAccountNo() {
+    	return accountNo;
+    }
+    
+    public void setAccountNo(String accountNo) {
+    	this.accountNo = accountNo; 
+    }
+    
+    public int getBalance() {
+    	return balance;
+    }
+    
+    public void setBalance(int balance) {
+    	this.balance = balance; 
+    }
+   
     //필요한 메소드 작성
+    
+    public void deposit(int balance) {
+    	this.balance += balance;
+    }
+    
+    public void withdraw(int balance) {
+    	this.balance -= balance;
+    }
+    
+    public void showBalance() {
+    	System.out.println(this.balance);
+    	
+    }
 
 }
